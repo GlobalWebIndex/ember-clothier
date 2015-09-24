01e740f58f7247d5ebf6b5905f1e42d827c35976
Marek Fajkus <marek.faj@gmail.com>
2015-09-24 18:13:02 +0200

    Refactor relation decorating

--- addon/model-mixin.js
+++ addon/model-mixin.js
@@ -20,49 +20,49 @@ export default Ember.Mixin.create({
   */
   decorate: function(alias) {
     return create.bind(this)(this, alias);
-  },
-
-  /*
-   * Decorate record in hasMany relation
-   * @param collcetionKey[String]
-   * @param alias[String]
-   * @return Array
-   * Usage:
-
-   // with default decorator:
-   childrens: DS.hasmany('childrens'),
-   decoratedChildrens: this.decorateHasMany('childrens')
-
-   // with specified decorator:
-   childrens: DS.hasmany('childrens'),
-   decoratedChildrens: this.decorateHasMany('childrens', 'decoratorName')
-  */
-  decorateHasMany: function(collectionKey, alias) {
-    return c(collectionKey, function() {
-      var collection = this.get(collectionKey);
-      createCollection.bind(this)(collection, alias);
-    });
-  },
-
-  /*
-   * Decorate record in belongsTo relation
-   * @param modelName[String]
-   * @param alias[String]
-   * @return Object
-   * USAGE:
-
-   // with default decorator:
-   childrens: DS.belongsTo('parent'),
-   decoratedParent: this.decorateBelongsTo('parent')
-
-   // with specified decorator:
-   childrens: ds.belongsTo('parent'),
-   decoratedParent: this.decoraBelonsTo('parent', 'decoratorName')
-  */
-  decorateBelongsTo: function(modelName, alias) {
-    return c(modelName, function() {
-      var model = this.get(modelName);
-      return create.bind(this)(model, alias);
-    });
   }
 });
+
+/*
+ * Decorate record in hasMany relation
+ * @param collcetionKey[String]
+ * @param alias[String]
+ * @return Array
+ * Usage:
+
+ // with default decorator:
+ childrens: DS.hasmany('childrens'),
+ decoratedChildrens: this.decorateHasMany('childrens')
+
+ // with specified decorator:
+ childrens: DS.hasmany('childrens'),
+ decoratedChildrens: decorateHasMany('childrens', 'decoratorName')
+*/
+export function decorateHasMany(collectionKey, alias) {
+  return c(collectionKey, function() {
+    var collection = this.get(collectionKey);
+    return createCollection.bind(this)(collection, alias);
+  });
+};
+
+/*
+ * Decorate record in belongsTo relation
+ * @param modelName[String]
+ * @param alias[String]
+ * @return Object
+ * USAGE:
+
+ // with default decorator:
+ childrens: DS.belongsTo('parent'),
+ decoratedParent: this.decorateBelongsTo('parent')
+
+ // with specified decorator:
+ childrens: DS.belongsTo('parent'),
+ decoratedParent: decoraBelonsTo('parent', 'decoratorName')
+*/
+export function decorateBelongsTo(modelName, alias) {
+  return c(modelName, function() {
+    var model = this.get(modelName);
+    return create.bind(this)(model, alias);
+  });
+};
