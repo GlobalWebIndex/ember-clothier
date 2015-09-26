118ab3b0cb4fcd85d54c0ad686d4519b00b4f525
Marek Fajkus <marek.faj@gmail.com>
2015-09-26 15:44:29 +0200

    setup ember-try, closes #6

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,50 +3,41 @@ module.exports = {
     {
       name: 'Ember 1.10 with ember-data',
       dependencies: {
-        'ember': '1.10',
+        'ember': '1.10.1',
         'ember-data': '1.0.0-beta.15'
       }
     },
     {
       name: 'Ember 1.11 with ember-data',
       dependencies: {
-        'ember': '1.11',
-        'ember-data': '1.0.0-beta.15'
+        'ember': '1.11.3',
+        'ember-data': '1.0.0-beta.19'
       }
     },
     {
       name: 'Ember 1.12 with ember-data',
       dependencies: {
         'ember': '1.12.1',
-        'ember-data': '1.0.0-beta.15'
+        'ember-data': '1.0.0-beta.19'
       }
     },
     {
       name: 'Ember 1.13 with ember-data',
       dependencies: {
-        'ember': '1.13.8',
-        'ember-data': '1.0.0-beta.15'
+        'ember': '1.13.10',
+        'ember-data': '1.13.13'
       }
     },
     {
       name: 'Ember 2.0 with ember-data',
       dependencies: {
-        'ember': '2.0.7',
-        'ember-data': '1.0.0-beta.15'
+        'ember': '2.0.2',
+        'ember-data': '2.0.1'
       }
     },
     {
       name: 'Ember canary',
       dependencies: {
-        'ember': 'canary'
-      }
-    },
-    {
-      name: 'Ember beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: { // Resolutions are only necessary when they do not match the version specified in `dependencies`
         'ember': 'canary'
       }
     }
