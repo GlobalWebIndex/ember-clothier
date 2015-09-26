dc103d83cf899609190f17aa9fb07807b1b823d6
Marek Fajkus <marek.faj@gmail.com>
2015-09-26 19:38:24 +0200

    refactor ember-try based on current support

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,13 +1,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'Ember 1.10 with ember-data',
-      dependencies: {
-        'ember': '1.10.1',
-        'ember-data': '1.0.0-beta.15'
-      }
-    },
-    {
       name: 'Ember 1.11 with ember-data',
       dependencies: {
         'ember': '1.11.3',
@@ -26,19 +19,6 @@ module.exports = {
       dependencies: {
         'ember': '1.13.10',
         'ember-data': '1.13.13'
-      }
-    },
-    {
-      name: 'Ember 2.0 with ember-data',
-      dependencies: {
-        'ember': '2.0.2',
-        'ember-data': '2.0.1'
-      }
-    },
-    {
-      name: 'Ember canary',
-      dependencies: {
-        'ember': 'canary'
       }
     }
   ]
