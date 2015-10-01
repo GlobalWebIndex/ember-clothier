c90dfffbb5d4a5079072ed9e01ec3bc626eabf23
Marek Fajkus <marek.faj@gmail.com>
2015-10-01 15:08:17 +0200

    init travis ci

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,21 +1,21 @@
 module.exports = {
   scenarios: [
     {
-      name: 'Ember 1.11 with ember-data',
+      name: 'ember-1.11-with-ember-data',
       dependencies: {
         'ember': '1.11.3',
         'ember-data': '1.0.0-beta.19'
       }
     },
     {
-      name: 'Ember 1.12 with ember-data',
+      name: 'ember-1.12-with-ember-data',
       dependencies: {
         'ember': '1.12.1',
         'ember-data': '1.0.0-beta.19'
       }
     },
     {
-      name: 'Ember 1.13 with ember-data',
+      name: 'ember-1.13-with-ember-data',
       dependencies: {
         'ember': '1.13.10',
         'ember-data': '1.13.13'
