60e6f2951db5f056d3ed4b2286c781a604f4aef2
Marek Fajkus <marek.faj@gmail.com>
2015-10-06 16:49:58 +0200

    Methods proxy from decorator to its content
    closes #23

--- addon/model-decorator.js
+++ addon/model-decorator.js
@@ -1,3 +1,40 @@
 import Ember from 'ember';
 
-export default Ember.ObjectProxy.extend();
+const o = Ember.observer;
+const on = Ember.on;
+
+var Decorator = Ember.ObjectProxy.extend({
+  /*
+   * Add method from content
+   * @param method[function]
+   * @return function
+   */
+  _addMethod(method){
+    this[method] = function() {
+      var content = this.get('content');
+
+      if (Ember.isEmpty(content[method])) {
+        return;
+      }
+
+      return content[method].apply(this, arguments);
+    };
+  },
+
+  /*
+   * Setup method proxying
+   */
+  _setupMethods: on('init', o('content', function(){
+    var content = this.get('content');
+
+    if (Ember.isEmpty(content)) { return; }
+
+    for (var method in content) {
+      if(Ember.typeOf(content[method]) === "function" && Ember.isEmpty(this[method])) {
+         this._addMethod(method);
+      }
+    }
+  }))
+});
+
+export default Decorator;
--- tests/integration/model-decorator-test.js
+++ tests/integration/model-decorator-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { module, test } from 'qunit';
 import Decorator from 'ember-clothier/model-decorator';
 
@@ -10,3 +11,35 @@ test('can create instance', function(assert) {
   assert.equal(decorator.get('content'), null, 'have no content by default');
 });
 
+test('method are proxyed to content', function(assert) {
+
+  var model = Ember.Object.create({
+    name: 'Petr',
+
+    sayHi: function() {
+      return 'Hi, ' + this.get('name') + '!';
+    },
+
+    iam: function() {
+      return 'model';
+    }
+  });
+
+  var decoratedModel = Decorator.create({
+    content: model,
+
+    iam: function() {
+      return 'decorator';
+    }
+  });
+
+  assert.equal(decoratedModel.sayHi(), 'Hi, Petr!', 'Model\'s method is accesible');
+  assert.equal(decoratedModel.iam(), 'decorator', 'Decorator can overwrite model\'s method');
+
+  Ember.run(() => {
+    decoratedModel.set('name', 'Zdenko');
+  });
+
+  assert.equal(decoratedModel.get('name'), 'Zdenko', 'Object property should be set');
+  assert.equal(decoratedModel.get('content.name'), 'Zdenko', 'Setter is proxied to model');
+});
